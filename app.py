3646d696fbeb089649a28e83b32ebb8ef929cb98
demidraksin <greatraksin@icloud.com>
2021-03-05 14:03:35 +0300

    🐳💨Adding custom error pages🐻‍❄️

--- app.py
+++ app.py
@@ -50,5 +50,10 @@ def delete(id):
         return 'There was a problem deleting data!'
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('not_found.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=8910)
